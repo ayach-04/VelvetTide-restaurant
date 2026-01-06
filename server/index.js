a0842d3f5c4e291ebee9e6053f2010f61e68af81
Aya <ilainjouri@gmail.com>
2026-01-07 00:30:06 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,13 @@ const adminRoutes = require("./routes/adminRoutes");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors(
+   {
+          origin : ["https://velvettide-backend.vercel.app"],
+          methods: ["POST", "GET"],
+          cerdentails: true
+   }
+));
 app.use(express.json({ limit: "25mb" }));
 
 app.get("/api/health", (_req, res) => {
