327bfdd5fe6e172ef01487d2cea3b714179a5fac
Aya <ilainjouri@gmail.com>
2026-03-11 17:10:22 +0100

    Fix image source path for Banner1.png

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -404,7 +404,7 @@ export default function Home() {
       >
         {/* Background Image */}
         <img
-          src="../../public /Photos/Banner1.png"
+          src="../../public/photos/Banner1.png"
           alt="Restaurant Banner"
           style={{
             position: "absolute",
